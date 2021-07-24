1d9b1a39efa113e3a932024ee81ee71d28a1d1ce
Sabrina Sumona <snsbauet04@gmail.com>
2021-07-24 23:39:03 +0600

    Shortcut icon

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="images/png" href="images/logo.png" sizes="16x16">
     <title>Happy Learning Log In</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
