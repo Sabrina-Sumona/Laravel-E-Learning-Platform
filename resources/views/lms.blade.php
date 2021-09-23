c23567829a58b6773008470c01b99058de8e4d41
Sabrina Sumona <snsbauet04@gmail.com>
2021-09-23 14:11:14 +0600

    Update

--- resources/views/lms.blade.php
+++ resources/views/lms.blade.php
@@ -17,7 +17,7 @@
     <div class="container mt-5">
       <div class="category mb-30">
         <div class="box">
-          <h5 class="colors1 mt-5 mb-2">Tasks</h5>
+          <h5 class="colors1 mt-5 mb-2">Assignment</h5>
           <hr class="colors1">
           <span class="colors2 mb-4  mt-5"><a href="{{route('tasks')}}">View Details</a></span>
         </div>
@@ -30,7 +30,7 @@
         <div class="box">
           <h5 class="colors1 mt-5 mb-2">Exam</h5>
           <hr class="colors1">
-          <span class="colors2 mb-4  mt-5"><a href="#">View Details</a></span>
+          <span class="colors2 mb-4  mt-5"><a href="{{route('tasks')}}">View Details</a></span>
         </div>
       </div>
     </div>
